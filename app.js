59c2ae469b02ca60c7d0584e917e5c5ce36d912f
fantoniog <fantonio@maxipublica.com>
2022-05-30 18:45:39 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+require('dotenv').config()
+const app = express()
+const port = process.env.PORT || 3020
+
+app.get('/', (req, res) => {
+    res.send("holaaaaaaaaaa");
+})
+
+app.listen(port, () => {
+    console.log(`Starting ${process.env.IP}${port}`);
+})
